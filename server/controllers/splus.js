370f30e77bd6ea463e89ff82853c267b10c361e6
schneefux <schneefux+commit@schneefux.xyz>
2018-10-15 15:46:10 +0200

    Use cache-manager for caching, support ttl

--- server/controllers/splus.js
+++ server/controllers/splus.js
@@ -1,24 +1,31 @@
 const express = require('express'),
   { createHash } = require('crypto'),
+  cacheManager = require('cache-manager'),
   router = express.Router(),
   { SplusApi } = require('../lib/SplusApi');
 
 const sha256 = (x) => createHash('sha256').update(x, 'utf8').digest('hex');
 
-const cache = new Map();
+const cache = cacheManager.caching({
+  store: 'memory',
+  max: 1000,
+  ttl: 10,
+});
 
 router.get('/:course/:week', async (req, res) => {
   const course = req.params.course;
   const week = parseInt(req.params.week);
   const key = `${course}-${week}`;
 
-  if (!cache.has(key)) {
-    const data = await SplusApi.getData('#' + course, week);
-    const id = (lecture) => sha256(JSON.stringify({ lecture, course, week }));
-    cache.set(key, data.map((lecture) => ({ ...lecture, course, week, id: id(lecture) })));
-  }
+  const data = await cache.wrap(key,
+    async () => await SplusApi.getData('#' + course, week),
+    { ttl: 600 }
+  );
+  console.log(data);
+  const id = (lecture) => sha256(JSON.stringify({ lecture, course, week }));
+  const result = data.map((lecture) => ({ ...lecture, course, week, id: id(lecture) }));
 
-  res.json(cache.get(key));
+  res.json(result);
 });
 
 module.exports = router;
