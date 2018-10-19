d7c0748b452c86c58dadeb75d251e61cb426aeae
schneefux <schneefux+commit@schneefux.xyz>
2018-10-19 13:24:36 +0200

    Split nuxt server (JS) and API server (TS)

--- server/controllers/splus.ts
+++ server/controllers/splus.ts
@@ -1,11 +1,11 @@
-const express = require('express'),
-  { createHash } = require('crypto'),
-  cacheManager = require('cache-manager'),
-  router = express.Router(),
-  { SplusApi } = require('../lib/SplusApi');
+import * as express from 'express';
+import { createHash } from 'crypto';
+import * as cacheManager from 'cache-manager';
+import { SplusApi } from '../lib/SplusApi';
 
 const sha256 = (x) => createHash('sha256').update(x, 'utf8').digest('hex');
 
+const router = express.Router();
 const cache = cacheManager.caching({
   store: 'memory',
   max: 1000,
@@ -17,14 +17,14 @@ router.get('/:course/:week', async (req, res) => {
   const week = parseInt(req.params.week);
   const key = `${course}-${week}`;
 
-  const data = await cache.wrap(key,
-    async () => await SplusApi.getData('#' + course, week),
-    { ttl: 600 }
-  );
+  const data = await cache.wrap(key, async () => {
+    console.log(`cache miss for key ${key}`);
+    return await SplusApi.getData('#' + course, week);
+  }, { ttl: 600 });
   const id = (lecture) => sha256(JSON.stringify({ lecture, course, week }));
   const result = data.map((lecture) => ({ ...lecture, course, week, id: id(lecture) }));
 
   res.json(result);
 });
 
-module.exports = router;
+export default router;
--- server/lib/SplusApi.ts
+++ server/lib/SplusApi.ts
@@ -1,18 +1,21 @@
-import request from 'axios';
-import { SplusParser } from './SplusParser';
-import { ILecture } from './ILecture';
+import * as request from 'request-promise-native';
+import {SplusParser} from './SplusParser';
+import {ILecture} from './ILecture';
 
 export class SplusApi {
     private static _base_uri = 'http://splus.ostfalia.de/semesterplan123.php';
 
-    private static async splusRequest(course: string, weekOfYear: number): Promise<string> {
-        const response = await request({
-            url: this._base_uri,
-            method: 'post',
-            params: { identifier: course },
-            data: { weeks: weekOfYear.toString() },
+    private static splusRequest(course: string, weekOfYear: number): PromiseLike<string> {
+        return request({
+            method: 'POST',
+            uri: this._base_uri,
+            qs: {
+                identifier: course,
+            },
+            formData: {
+                weeks: weekOfYear.toString(),
+            },
         });
-        return response.data;
     }
 
     static async getData(course: string, weekOfYear: number): Promise<ILecture[]> {
