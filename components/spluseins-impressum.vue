5856cdedfbdfe6e3eafe30b0bc61b11d4f0f9b80
schneefux <schneefux+github@schneefux.xyz>
2018-10-25 19:26:53 +0200

    antispambots
    Co-Authored-By: hidotp <janfasel@gmx.net>

--- components/spluseins-impressum.vue
+++ components/spluseins-impressum.vue
@@ -39,7 +39,7 @@
 
           <p><b>Kontakt:</b></p>
 
-          <p>E-Mail: team@spluseins.de <b>Haftungsausschluss: Haftung für Inhalte</b></p>
+          <p>E-Mail: team (at) spluseins (Punkt) de <b>Haftungsausschluss: Haftung für Inhalte</b></p>
 
           <p>Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen. </p>
 
