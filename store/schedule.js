ebe37bb710f1f8eab84d4cb4d76cd65821dbd333
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2018-10-29 21:45:46 +0100

    Display only weekdays with lectures (#19)

--- store/schedule.js
+++ store/schedule.js
@@ -1,9 +1,13 @@
 export const state = () => ({
   schedule: {},
+  hasLecturesOnWeekend: false, 
 });
 
 export const mutations = {
   setSchedule(state, schedule) {
     state.schedule = schedule;
+  },
+  setHasLecturesOnWeekend(state, hasLectures){
+    state.hasLecturesOnWeekend = hasLectures;
   }
 };
