11cdfddfab32cd59b76cbd76858a384ef67ced3a
schneefux <schneefux+commit@schneefux.xyz>
2018-10-30 13:54:46 +0100

    Refactor lecture caching
      * api: Added Cache-Control header
        This allows the browser to cache API responses.
      * api: Added CORS header
        Browser can now call the API from any domain
        which makes development and testing easier.
      * store: Merged modules into single splus module
        All attributes depended on each other so it made sense to unify it.
      * store: Moved lecture to event conversion logic from component to getter
        Getters are cached and were easier to debug.
      * store: Replaced schedule & week cache by schedule cache
        Since API data is cached by the browser, aggressive store caching
        is not needed anymore. Keep the week cache because dayspan-vuetify
        has performance issues for a large number of events
        and week pagination should be as smooth as possible.
      * calendar: Simplified event lifecycle
        Events can be rendered server side.

--- store/splus.js
+++ store/splus.js
@@ -1,28 +1,98 @@
+import Vue from 'vue';
+import colors from 'vuetify/es5/util/colors';
+import * as moment from 'moment';
 import SCHEDULES from '~/assets/schedules.json';
 
+const hashCode = (string) =>
+string.split('').reduce((prevHash, currVal) =>
+  (((prevHash << 5) - prevHash) + currVal.charCodeAt(0))|0, 0);
+
+
 export const state = () => ({
-  lectures: [],
+  schedule: SCHEDULES[0],
   schedules: SCHEDULES,
+  /**
+   * Map of { week: lectures[] }
+   */
+  lectures: {},
+  /**
+   * Currently viewed iso week of year.
+   * Week 53 of year 2018 equals week 1 of year 2019.
+   */
+  week: moment().isoWeek(), // TODO won't work in 2019
 });
 
 export const getters = {
-  getLecturesByWeekAndSchedule: (state) => (week, schedule) => {
-    return state.lectures.filter((lecture) =>
-      lecture.week == week && lecture.schedule == schedule);
+  getHasLecturesOnWeekend: (state) => {
+    if (state.lectures[state.week] == undefined) {
+      return false;
+    }
+
+    // 0: Monday, … 4: Friday, 5: Saturday, 6: Sunday
+    return state.lectures[state.week].filter(lecture => lecture.day > 4).length > 0;
+  },
+  /**
+   * @return The lectures as timestamp-aware dayspan calendar event inputs.
+   * @see https://clickermonkey.github.io/dayspan/docs/interfaces/eventinput.html
+   */
+  getLecturesAsEvents: (state) => {
+    if (state.lectures[state.week] == undefined) {
+      return [];
+    }
+
+    const colorsArr = Object.values(colors).slice(0, -1); // exclude black
+
+    return state.lectures[state.week].map((lecture) => {
+      const beginHours = Math.floor(lecture.begin);
+      const start = moment()
+        .isoWeek(lecture.week)
+        .day(lecture.day + 1)
+        .hour(beginHours)
+        .minute((lecture.begin - beginHours) * 60);
+      const hashOfFirstWordInTitle = hashCode(lecture.title.split(' ')[0]) + Math.pow(2, 31);
+      const color = colorsArr[hashOfFirstWordInTitle % colorsArr.length].lighten1;
+
+      return {
+        data: {
+          title: lecture.title,
+          color,
+          description: `${lecture.lecturer} ${lecture.room} ${lecture.info}`,
+          location: lecture.room,
+        },
+        schedule: {
+          on: start,
+          times: [ {
+            hour: start.hour(),
+            minute: start.minute(),
+          } ],
+          duration: lecture.end - lecture.begin,
+          durationUnit: 'hours',
+        }
+      };
+    });
   },
 };
 
 export const mutations = {
-  addLectures(state, lectures) {
-    lectures
-      .filter((lecture) => !state.lectures.find((lecture2) => lecture.id == lecture2.id))
-      .forEach((lecture) => state.lectures.push(lecture));
+  addLectures(state, { lectures, week }) {
+    // reactive variant of state.lectures[week] = lectures
+    Vue.set(state.lectures, week, lectures);
+  },
+  setWeek(state, week) {
+    state.week = week;
+  },
+  /**
+   * Set the schedule and clear the cache.
+   */
+  setSchedule(state, schedule) {
+    state.lectures = {};
+    state.schedule = schedule;
   },
 };
 
 export const actions = {
-  async load({ state, commit }, { schedule, week }) {
-    const response = await this.$axios.get(`/api/splus/${schedule}/${week}`);
-    commit('addLectures', response.data);
+  async load({ state, commit }) {
+    const response = await this.$axios.get(`/api/splus/${state.schedule.id}/${state.week}`);
+    commit('addLectures', { lectures: response.data, week: state.week });
   },
 };
