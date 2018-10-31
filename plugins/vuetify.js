8959ac55a1381f5eeea18bff81103cd41fd25612
schneefux <schneefux+commit@schneefux.xyz>
2018-10-31 21:56:35 +0100

    vuetify: Enable custom properties

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -13,6 +13,9 @@ Vue.$dayspan.addLocale('de', LocaleDe);
 Vue.$dayspan.setLocale('de', true);
 
 Vue.use(Vuetify, {
+  options: {
+    customProperties: true,
+  },
   theme: {
     primary: colors.amber.base,
     accent: colors.lightBlue.base,
