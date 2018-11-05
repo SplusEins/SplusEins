2a53072fecf9f8146837299c9e0d8f8bd5d85c63
hi.p <janfasel@gmx.net>
2018-11-05 23:24:11 +0100

    icon pushed and added but wtf

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: pkg.description }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: '/smolIcon.ico' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' }
     ]
   },
