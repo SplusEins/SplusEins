deec5afbbcd47d51e7f896f508ebeb75fc36803a
schneefux <schneefux+commit@schneefux.xyz>
2018-11-07 16:07:32 +0000

    Add error handling to UI

---
+++ components/spluseins-snackbar.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-snackbar
+    v-model="snackbarOpen"
+    color="error"
+    right>
+    {{ message }}
+    <v-btn
+      dark
+      flat
+      @click="clearError()">
+      Schließen
+    </v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex';
+
+export default {
+  name: 'SpluseinsSnackbar',
+  computed: {
+    snackbarOpen: {
+        get() {
+            return !!this.message;
+        },
+        set() {
+            this.clearError();
+        }
+    },
+    ...mapState({
+      message: state => state.splus.error,
+    }),
+  },
+  methods: {
+    ...mapMutations({
+      clearError: 'splus/clearError',
+    }),
+  },
+};
+</script>
