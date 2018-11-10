efd546110533d2bdfd24fb5c066d7e54268e3d79
schneefux <schneefux+commit@schneefux.xyz>
2018-11-10 18:25:29 +0000

    Impressum: Add test

---
+++ components/__tests__/spluseins-impressum.spec.ts
@@ -0,0 +1,42 @@
+import { mount } from '@vue/test-utils';
+import * as Vue_ from 'vue';
+import * as Vuetify_ from 'vuetify';
+import SpluseinsImpressum from '../spluseins-impressum.vue';
+
+const Vue: any = Vue_;
+const Vuetify: any = Vuetify_;
+
+Vue.use(Vuetify);
+
+describe('Impressum Dialog', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = mount(SpluseinsImpressum);
+  });
+
+  it('should render no dialog by default', () => {
+    expect(wrapper.contains('.v-dialog--active')).toBeFalsy();
+  });
+
+  it('should render a dialog after clicking the link', () => {
+    wrapper.find('a').trigger('click');
+    expect(wrapper.contains('.v-dialog--active')).toBeTruthy();
+  });
+
+  it('should render no dialog after dismissing it', () => {
+    wrapper.find('a').trigger('click');
+    wrapper.find('.v-btn').trigger('click');
+    expect(wrapper.contains('.v-dialog--active')).toBeFalsy();
+  });
+
+  it('should contain a mail address', () => {
+    wrapper.find('a').trigger('click');
+    expect(wrapper.text()).toMatch('E-Mail:');
+  });
+
+  it('should contain real name placeholders', () => {
+    wrapper.find('a').trigger('click');
+    expect(wrapper.text()).toMatch('$Teammitglied1');
+  });
+});
