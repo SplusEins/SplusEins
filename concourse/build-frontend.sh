044e16320a56bf1b271e629248a9eb66a8f23318
BrainConverter <fabian.pfuetsch@gmail.com>
2018-11-11 12:10:47 +0100

    api url to master.spluseins.de

--- concourse/build-frontend.sh
+++ concourse/build-frontend.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd npm-install-result
-API_URL=https://spluseins.de/ npm run build
+API_URL=https://master.spluseins.de/ npm run build
 cd ..
 cp -R npm-install-result/. frontend-build-result/
