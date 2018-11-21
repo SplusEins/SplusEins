a14a03ee2c425e213027d70fff231273db914ded
BrainConverter <fabian.pfuetsch@gmail.com>
2018-11-21 19:44:42 +0100

    add favorite via timetable icon

--- components/favorite-timetables-list.vue
+++ components/favorite-timetables-list.vue
@@ -4,16 +4,8 @@
 
     <v-subheader>
       Favoriten
-      <v-btn
-        icon
-        flat
-        @click="favoriteTimetableDialogOpen = true">
-        <v-icon>menu</v-icon>
-      </v-btn>
     </v-subheader>
-    
-    <favorite-timetable-dialog v-model="favoriteTimetableDialogOpen" />
-  
+      
     <v-list-tile
       v-for="schedule in favoriteSchedules"
       :to="scheduleToRoute(schedule)"
@@ -30,18 +22,9 @@
 
 <script lang="js">
   import { mapState } from 'vuex';
-  import FavoriteTimetableDialog from './favorite-timetable-dialog.vue';
   
   export default  {
     name: 'FavoriteTimetablesList',
-    components: {
-      FavoriteTimetableDialog,
-    },
-    data() {
-      return {
-        favoriteTimetableDialogOpen: false,
-      };
-    },
     computed: {
       ...mapState({
         favoriteSchedules: (state) => state.splus.favoriteSchedules,
