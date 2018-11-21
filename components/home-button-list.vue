69231fc7e309275f641ae76b153ac81a85698801
schneefux <schneefux+commit@schneefux.xyz>
2018-11-21 13:58:18 +0000

    Add button for home page

---
+++ components/home-button-list.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-list>
+    <v-list-tile
+      to="/"
+      nuxt>
+      <v-list-tile-content>
+        <v-list-tile-title>Startseite</v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+  </v-list>
+</template>
+
+<script>
+export default { };
+</script>
