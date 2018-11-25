9dd463fdfac61dadf584f1136d99c0bceaed54e8
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2018-11-25 14:09:46 +0100

    Mensa plan setup (#88)

--- components/basic-utilities-list.vue
+++ components/basic-utilities-list.vue
@@ -26,19 +26,35 @@
       </v-list-tile-content>
     </v-list-tile>
 
+    <v-list-tile @click="mensaPlanDialogOpen = true">
+      <v-list-tile-action>
+        <v-icon>fastfood</v-icon>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>
+          Mensa Plan Wolfenbüttel
+        </v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+
     <custom-timetable-dialog v-model="customTimetableDialogOpen" />
+    <mensa-plan-dialog v-model="mensaPlanDialogOpen" />
   </v-list>
 </template>
 
 <script>
 import CustomTimetableDialog from './custom-timetable-dialog.vue';
+import MensaPlanDialog from './mensa-plan-dialog.vue';
+
 export default {
   components: {
     CustomTimetableDialog,
+    MensaPlanDialog,
   },
   data() {
     return {
       customTimetableDialogOpen: false,
+      mensaPlanDialogOpen: false,
     };
   },
   methods:{
