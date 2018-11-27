dff9530c465c97a5b08b7d2d4cbe4ae5da533723
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2018-11-27 14:12:46 +0100

    Mensa Plan own page (#100)
    * move mensa plan to a page and change plan loading

--- store/mensa.js
+++ store/mensa.js
@@ -7,25 +7,36 @@ export const state = () => ({
 export const mutations = {
   setWeekPlan(state, data){
     state.weekPlan = data;
-  }
+  },
 }
 
 export const actions = {
   async loadWeek({ state, commit }) {
+    let openDays = [];
     let weekdays = [];
     let result = [];
 
-    for(let i = 0; i<5; i++) {
-        weekdays.push(moment().startOf('isoWeek').add(i,'days'));
+    try {
+      const response = await this.$axios.get(`https://openmensa.org/api/v2/canteens/166/days`);
+      openDays = response.data;
+    } catch (error) {
+        console.error('error during Mensa API call', error.message);
     }
+
+    for(let i=0; i<3; i++) {
+        weekdays.push(moment(openDays[i].date));
+    }
+
     await Promise.all(weekdays.map(async (day) => {
         try {
             const response = await this.$axios.get(`https://openmensa.org/api/v2/canteens/166/days/${day.format('YYYY-MM-DD')}/meals`);
-            result[day.day()-1] = {date: day, data: {...response.data}};
+            result.push({date: day, data: {...response.data}});
         } catch (error) {
             console.error('error during Mensa API call', error.message);
         }
     }));
+
+    result = result.sort((a,b) => a.date.day() > b.date.day());
 
     commit('setWeekPlan', result);
   }
