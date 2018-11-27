af63ae24ae8099aac25962fa0135100ba428423c
hi.p <janfasel@gmx.net>
2018-11-27 16:54:09 +0100

    error page ist daaaa
    - no copyright lel

---
+++ layouts/error.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-app :dark="isDark">
+    <spluseins-header/>
+    <v-content fill-height>
+      <v-layout 
+        align-center 
+        justify-center 
+        column 
+        fill-height>
+        <v-card
+          pa-2
+          ma-2
+          max-width="60%"
+        >
+          <v-card-media
+            src="/error.jpeg"
+          />
+          <v-card-text>
+            <div>OOPSIE WOOPSIE!! UwU We made a fucky wucky!! A wittle fucko boingo! The code monkeys at our headquaters are working VEWY HAWD to fix this.</div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn 
+              flat
+              to="/"
+              nuxt
+              color="amber"
+              @click="trackMatomoEvent('Error','goBackToMainPage','with error:' + error.statusCode)">
+              Main Page
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-layout>
+    </v-content>
+    <spluseins-footer/>
+  </v-app>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+import SpluseinsHeader from '../components/spluseins-header.vue';
+import SpluseinsFooter from '../components/spluseins-footer.vue';
+
+export default {
+  components: {
+    SpluseinsHeader,
+    SpluseinsFooter,
+  },
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    ...mapState({
+      isDark: state => state.theme.isDark,
+    }),
+  },  
+  methods: {
+    trackMatomoEvent (category, action, name) {
+      this.$matomo.trackEvent(category, action, name);
+    },
+  },
+}
+</script>
