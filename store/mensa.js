f0de7b6ae877b63257bf9029fe9a2c88f259c4b2
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2018-11-27 20:37:16 +0100

    Integrate OpenMensa API call in our API (#112)

--- store/mensa.js
+++ store/mensa.js
@@ -1,5 +1,3 @@
-import * as moment from 'moment';
-
 export const state = () => ({
   weekPlan: [],
 });
@@ -12,31 +10,15 @@ export const mutations = {
 
 export const actions = {
   async loadWeek({ state, commit }) {
-    let openDays = [];
-    let weekdays = [];
     let result = [];
 
     try {
-      const response = await this.$axios.get(`https://openmensa.org/api/v2/canteens/166/days`);
-      openDays = response.data;
+      const response = await this.$axios.get(`/api/mensa`);
+      result = response.data;
     } catch (error) {
+        commit('setError', 'API-Verbindung fehlgeschlagen');
         console.error('error during Mensa API call', error.message);
     }
-
-    for(let i=0; i<3; i++) {
-        weekdays.push(moment(openDays[i].date));
-    }
-
-    await Promise.all(weekdays.map(async (day) => {
-        try {
-            const response = await this.$axios.get(`https://openmensa.org/api/v2/canteens/166/days/${day.format('YYYY-MM-DD')}/meals`);
-            result.push({id: day.month() + day.day(), date: day.format('YYYY-MM-DD'), data: {...response.data}});
-        } catch (error) {
-            console.error('error during Mensa API call', error.message);
-        }
-    }));
-
-    result = result.sort((a,b) => a.id > b.id);
 
     commit('setWeekPlan', result);
   }
