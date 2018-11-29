c53f2b774ea7a5ab4a8f624d3095195f8857c811
hi.p <38942179+hidotp@users.noreply.github.com>
2018-11-29 18:11:43 +0100

    Additional Matomo tracking  (#117)
    * Anzahl Pläne + Anzahl Kurse werden getrackt
    
    * seperate courses and schedules

--- components/custom-timetable-dialog.vue
+++ components/custom-timetable-dialog.vue
@@ -22,7 +22,7 @@
             :disabled="!valid"
             dark
             flat
-            @click.native="save()">Speichern</v-btn>
+            @click.native="save(); trackMatomoEvent('Menu','saveCustomTimetable',' Anzahl Pläne:',selectedSchedules.length ); trackMatomoEvent('Menu','saveCustomTimetable','Anzahl Kurse:', selectedCourses.length)">Speichern</v-btn>
         </v-toolbar-items>
       </v-toolbar>
       <v-form v-model="valid">
@@ -148,6 +148,9 @@ export default {
     }
   },
   methods: {
+    trackMatomoEvent (category, action , name, value) {
+      this.$matomo.trackEvent(category, action, name,value);
+    },
     async addSchedule(schedule) {
       if (this.selectedSchedules.includes(schedule)) {
         return;
