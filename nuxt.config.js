f28c6cca1508071ac399afc36498f73a476c68da
schneefux <schneefux+commit@schneefux.xyz>
2018-12-02 11:16:10 +0000

    nuxt config: Fix typo

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,8 +8,8 @@ export default {
   */
   head: {
     titleTemplate: '%s - SplusEins',
-    link: [		
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700ç' },
+    link: [
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700' },
     ]
   },
 
