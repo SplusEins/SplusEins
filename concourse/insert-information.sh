037f2630fe3fec19744c45bf5e2d3f8919ad2c50
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-02-19 17:42:27 +0100

    print env variables

--- concourse/insert-information.sh
+++ concourse/insert-information.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 cd build-result/assets
+pwd
+printenv
 sed -i "s/Teammitglied1/$TEAM_MEMBER1/g" protected-information.json
 sed -i "s/Teammitglied2/$TEAM_MEMBER2/g" protected-information.json
 sed -i "s/Teammitglied3/$TEAM_MEMBER3/g" protected-information.json
