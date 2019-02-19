f09fe87d53ab74163dffdc014dbfeeddf01ad7d4
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-02-19 17:49:46 +0100

    test sed without env var

--- concourse/insert-information.sh
+++ concourse/insert-information.sh
@@ -2,7 +2,8 @@
 cd build-result/assets
 pwd
 printenv
-sed -i "s/Teammitglied1/$TEAM_MEMBER1/g" protected-information.json
+sed
+sed -i "s/Teammitglied1/TestMember1/g" protected-information.json
 sed -i "s/Teammitglied2/$TEAM_MEMBER2/g" protected-information.json
 sed -i "s/Teammitglied3/$TEAM_MEMBER3/g" protected-information.json
 sed -i "s/Teammitglied4/$TEAM_MEMBER4/g" protected-information.json
