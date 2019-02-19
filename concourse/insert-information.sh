caf584a261051053a546723a4f836424b6cc38ea
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-02-19 18:11:41 +0100

    insert information before build

--- concourse/insert-information.sh
+++ concourse/insert-information.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd npm-install-result/assets
-sed -i "s/Teammitglied1/TestMember1/g" protected-information.json
+sed -i "s/Teammitglied1/$TEAM_MEMBER1/g" protected-information.json
 sed -i "s/Teammitglied2/$TEAM_MEMBER2/g" protected-information.json
 sed -i "s/Teammitglied3/$TEAM_MEMBER3/g" protected-information.json
 sed -i "s/Teammitglied4/$TEAM_MEMBER4/g" protected-information.json
