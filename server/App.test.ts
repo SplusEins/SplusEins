666c2d9f079be94a49ce7ab84c59830ebd6de6fd
schneefux <schneefux+commit@schneefux.xyz>
2019-03-06 14:33:11 +0100

    Unify splus API cache

--- server/App.test.ts
+++ server/App.test.ts
@@ -3,31 +3,32 @@ process.env.CACHE_DISABLE = '1';
 import App from './App';
 import * as request from 'supertest';
 
-import { SplusApi } from './lib/SplusApi';
 import { SplusParser } from './lib/SplusParser';
-import { ILecture } from './lib/ILecture';
 import { readFile } from 'fs';
 import { promisify } from 'util';
+import { RichLecture } from '../model/RichLecture';
+
+async function splusApiMock(identifier: string, weekOfYear: string) {
+  const htmlPath = './server/__snapshots__/splus_ibi1_44.html';
+  const html = await promisify(readFile)(htmlPath, 'utf8');
+  const lectures = new SplusParser(html).getLectures();
+  return lectures.map((lecture) => new RichLecture(lecture, 12));
+}
+
+jest.mock('./lib/SplusApi', () => ({
+  default: jest.fn().mockImplementation(splusApiMock),
+}));
 
 describe('Test backend', () => {
   const app = new App().app;
 
-  async function splusMock(identifier: string,
-                           weekOfYear: string): Promise<ILecture[]> {
-    const htmlPath = './server/__snapshots__/splus_ibi1_44.html';
-    const html = await promisify(readFile)(htmlPath, 'utf8');
-    return new SplusParser(html).getLectures();
-  }
-
   it('should return parsed lectures', async () => {
-    SplusApi.getData = jest.fn().mockImplementationOnce(splusMock);
     const response = await request(app).get(
       '/api/splus/SPLUS7A3292/10');
     expect(response.body).toMatchSnapshot();
   });
 
   it('should create an ICS for multiple parameters', async () => {
-    SplusApi.getData = jest.fn().mockImplementation(splusMock);
     const response = await request(app).get(
       '/api/ics/v1/SPLUS7A3292,SPLUS7A3293/GdPL,DSB');
     expect(response.statusCode).toBe(200);
@@ -40,7 +41,6 @@ describe('Test backend', () => {
   });
 
   it('should create an ICS for one parameter', async () => {
-    SplusApi.getData = jest.fn().mockImplementation(splusMock);
     const response = await request(app).get(
       '/api/ics/v1/SPLUS7A3292/GdPL');
     expect(response.statusCode).toBe(200);
--- server/controllers/splus.ts
+++ server/controllers/splus.ts
@@ -1,29 +1,12 @@
 import * as express from 'express';
 import * as cors from 'cors';
-import * as cacheManager from 'cache-manager';
-import * as fsStore from 'cache-manager-fs-hash';
 import * as TIMETABLES from '../../assets/timetables.json';
 
-import { SplusApi } from '../lib/SplusApi';
-import { RichLecture } from '../../model/RichLecture';
+import getLectures from '../lib/SplusApi';
 
-const SCHEDULE_CACHE_SECONDS = 600;
-
-// default must be in /tmp because the rest is RO on AWS Lambda
-const CACHE_PATH = process.env.CACHE_PATH || '/tmp/spluseins-cache';
-const CACHE_DISABLE = !!process.env.CACHE_DISABLE;
+const SPLUS_CACHE_SECONDS = parseInt(process.env.SPLUS_CACHE_SECONDS || '600');
 
 const router = express.Router();
-const cache = CACHE_DISABLE ?
-  cacheManager.caching({ store: 'memory', max: 0 }) :
-  cacheManager.caching({
-    store: fsStore,
-    options: {
-      path: CACHE_PATH,
-      ttl: 60,
-      subdirs: true,
-    },
-  });
 
 /**
  * Accept CORS preflight requests.
@@ -46,16 +29,10 @@ router.get('/:timetable/:week', cors(), async (req, res, next) => {
   }
 
   const week = parseInt(req.params.week);
-  const key = `${timetableId}-${week}`;
 
   try {
-    const data = await cache.wrap(key, async () => {
-      console.log(`timetable cache miss for key ${key}`);
-      const lectures = await SplusApi.getData('#' + timetableId, week, timetable.setplan);
-      return lectures.map((ilecture) => new RichLecture(ilecture, week));
-    }, { ttl: SCHEDULE_CACHE_SECONDS });
-
-    res.set('Cache-Control', `public, max-age=${SCHEDULE_CACHE_SECONDS}`);
+    const data = await getLectures(timetable.id, week, timetable.setplan);
+    res.set('Cache-Control', `public, max-age=${SPLUS_CACHE_SECONDS}`);
     res.json(data);
   } catch (error) {
     next(error);
