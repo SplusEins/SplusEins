d8abb3df2dc51186efd1d7daa6891ba020c6ddeb
schneefux <schneefux+commit@schneefux.xyz>
2019-03-18 09:26:00 +0100

    Lazy load select dialog

--- components/select-dialog.vue
+++ components/select-dialog.vue
@@ -1,45 +1,50 @@
 <template>
-  <v-dialog
-    v-model="dialogOpen"
-    max-width="400"
+  <lazy-hydrate
+    ssr-only
+    :trigger-hydration="dialogOpen"
   >
-    <v-card>
-      <v-toolbar
-        dark
-        color="primary"
-      >
-        <v-btn
-          icon
+    <v-dialog
+      v-model="dialogOpen"
+      max-width="400"
+    >
+      <v-card>
+        <v-toolbar
           dark
-          @click.native="dialogOpen = false"
+          color="primary"
         >
-          <v-icon>mdi-close</v-icon>
-        </v-btn>
-        <v-toolbar-title>{{ title }}</v-toolbar-title>
-      </v-toolbar>
-
-      <v-card-text class="card-text-padding">
-        <v-list>
-          <v-list-tile
-            v-for="item in items"
-            :key="!!item.description? item.description: item.label"
-            @click="selectedItem = item, dialogOpen = false"
+          <v-btn
+            icon
+            dark
+            @click.native="dialogOpen = false"
           >
-            <v-list-tile-action>
-              <v-icon
-                v-if="!!item.description? item.description == selectedItem.description: item.label == selectedItem.label"
-              >
-                mdi-check
-              </v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ !!item.description? item.description: item.label }}</v-list-tile-title>
-            </v-list-tile-content>
-          </v-list-tile>
-        </v-list>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+        </v-toolbar>
+
+        <v-card-text class="card-text-padding">
+          <v-list>
+            <v-list-tile
+              v-for="item in items"
+              :key="!!item.description? item.description: item.label"
+              @click="selectedItem = item, dialogOpen = false"
+            >
+              <v-list-tile-action>
+                <v-icon
+                  v-if="!!item.description? item.description == selectedItem.description: item.label == selectedItem.label"
+                >
+                  mdi-check
+                </v-icon>
+              </v-list-tile-action>
+              <v-list-tile-content>
+                <v-list-tile-title>{{ !!item.description? item.description: item.label }}</v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+          </v-list>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </lazy-hydrate>
 </template>
 
 <script>
