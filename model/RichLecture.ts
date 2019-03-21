35108481e903de318aac07f02214ccdaf20081a6
schneefux <schneefux+commit@schneefux.xyz>
2019-03-21 20:53:30 +0100

    Fix other title ID collisions

--- model/RichLecture.ts
+++ model/RichLecture.ts
@@ -18,8 +18,60 @@ export class RichLecture {
 
   private generateTitleId(title: string): string {
     switch (title) {
+      case 'IE-Wissensmanagement':
+        return 'IW';
       case 'ITM-Wirtschaftsrecht':
         return 'IW2';
+      case 'Betriebssysteme':
+        return 'B';
+      case 'Batteriesysteme':
+        return 'B2';
+      case 'BWL':
+        return 'B3';
+      case 'FDynReg':
+        return 'F1';
+      case 'FEM':
+        return 'F2';
+      case 'Festigkeitslehre':
+        return 'F3';
+      case 'Ingeniurmathematik':
+        return 'I1';
+      case 'Internetprotokolle':
+        return 'I2';
+      case 'KonstKunst':
+        return 'K1';
+      case 'KostGerKons':
+        return 'K2';
+      case 'MathePLUS':
+        return 'M1';
+      case 'MDyn':
+        return 'M2';
+      case 'MikroC':
+        return 'M3';
+      case 'Physik':
+        return 'P1';
+      case 'Projektmanagement':
+        return 'P2';
+      case 'Recht':
+        return 'R1';
+      case 'Regelungstechnik':
+        return 'R2';
+      case 'RegTechV':
+        return 'R3';
+      case 'Sachenrecht':
+        return 'S1';
+      case 'SchaltT':
+        return 'S2';
+      case 'SenMessDV':
+        return 'S3';
+      case 'Sim':
+        return 'S4';
+      case 'SteuerT':
+        return 'S5';
+      case 'Thermodynamik':
+        return 'T1';
+      case 'Tribologie':
+        return 'T2';
     }
     return (title.match(/\b(\w)/g) || []).join('');
   }
