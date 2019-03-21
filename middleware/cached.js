1df901303ae7fa2a708d238c827f476ede816b57
schneefux <schneefux+commit@schneefux.xyz>
2019-03-21 19:29:05 +0100

    Only send cache header for empty error queue

--- middleware/cached.js
+++ middleware/cached.js
@@ -1,7 +1,7 @@
 const PAGE_CACHE_SECONDS = process.env.PAGE_CACHE_SECONDS || '600';
 
-export default function ({ res }) {
-  if (process.server) {
+export default function ({ res, store }) {
+  if (process.server && store.state.errorQueue.length == 0) {
     res.setHeader('Cache-Control', `public, max-age=${PAGE_CACHE_SECONDS}`);
   }
 }
