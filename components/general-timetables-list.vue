0e0ab6574d06d087f3ad860a6e3e4d4f171a2dc0
schneefux <schneefux+commit@schneefux.xyz>
2019-03-24 13:31:39 +0100

    General timetables list: Reduce # of DOM nodes

--- components/general-timetables-list.vue
+++ components/general-timetables-list.vue
@@ -10,9 +10,7 @@
       no-action
     >
       <v-list-tile slot="activator">
-        <v-list-tile-content>
-          <v-list-tile-title>{{ path }}</v-list-tile-title>
-        </v-list-tile-content>
+        {{ path }}
       </v-list-tile>
 
       <lazy-hydrate
@@ -26,17 +24,9 @@
           sub-group
         >
           <v-list-tile slot="activator">
-            <v-list-tile-content>
-              <v-list-tile-title v-if="semester == 'WPF'">
-                Wahlpflichtfächer
-              </v-list-tile-title>
-              <v-list-tile-title v-else-if="semester == 'OTHER'">
-                Sonstiges
-              </v-list-tile-title>
-              <v-list-tile-title v-else>
-                {{ semester }}. Semester
-              </v-list-tile-title>
-            </v-list-tile-content>
+            {{ semester == 'WPF' ? 'Wahlpflichtfächer' : '' }}
+            {{ semester == 'OTHER' ? 'Sonstiges' : '' }}
+            {{ !['WPF', 'OTHER'].includes(semester) ? semester + '. Semester' : ''}}
           </v-list-tile>
 
           <v-list-tile
@@ -44,15 +34,9 @@
             :key="schedule.id"
             :to="schedule.route"
             nuxt
+            @click="$track('Calendar', 'sideMenu plan used', 'normal')"
           >
-            <v-list-tile-content
-              value="true"
-              @click="$track('Calendar', 'sideMenu plan used', 'normal')"
-            >
-              <v-list-tile-title value="true">
-                {{ schedule.label }}
-              </v-list-tile-title>
-            </v-list-tile-content>
+            {{ schedule.label }}
           </v-list-tile>
         </v-list-group>
 
@@ -61,15 +45,9 @@
           :key="semester"
           :to="schedules[0].route"
           nuxt
+          @click="$track('Calendar', 'sideMenu plan used', 'normal')"
         >
-          <v-list-tile-content>
-            <v-list-tile-title v-if="semester == 'WPF'">
-              Wahlpflichtfächer
-            </v-list-tile-title>
-            <v-list-tile-title v-else>
-              {{ semester }}. Semester
-            </v-list-tile-title>
-          </v-list-tile-content>
+          {{ semester == 'WPF' ? 'Wahlpflichtfächer' : semester + '. Semester' }}
         </v-list-tile>
       </lazy-hydrate>
     </v-list-group>
@@ -80,11 +58,11 @@
 import { mapGetters } from 'vuex';
 
 export default {
-name: 'GeneralTimetablesList',
-computed: {
-  ...mapGetters({
-    schedulesTree: 'splus/getSchedulesAsTree',
-  }),
-},
+  name: 'GeneralTimetablesList',
+  computed: {
+    ...mapGetters({
+      schedulesTree: 'splus/getSchedulesAsTree',
+    }),
+  },
 };
 </script>
