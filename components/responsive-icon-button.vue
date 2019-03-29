025f5474a171a5ff28a56afee67bbd25e1655628
schneefux <schneefux+commit@schneefux.xyz>
2019-03-29 13:17:07 +0100

    Make responsive icon buttons non functional

--- components/responsive-icon-button.vue
+++ components/responsive-icon-button.vue
@@ -1,32 +1,29 @@
-<template functional>
+<template>
   <v-tooltip
-    :disabled="props.breakpoint"
+    :disabled="breakpoint"
     bottom
   >
     <v-btn
       slot="activator"
-      :outline="props.breakpoint"
-      :icon="!props.breakpoint"
-      :flat="!props.breakpoint"
-      @click="listeners.click"
+      :outline="breakpoint"
+      :icon="!breakpoint"
+      :flat="!breakpoint"
+      @click="emit('click')"
     >
-      <v-icon :left="props.breakpoint">
-        {{ props.icon }}
+      <v-icon :left="breakpoint">
+        {{ icon }}
       </v-icon>
-      <span v-show="props.breakpoint">
-        {{ props.text }}
+      <span v-show="breakpoint">
+        {{ text }}
       </span>
     </v-btn>
-    <span>{{ props.text }}</span>
+    <span>{{ text }}</span>
   </v-tooltip>
 </template>
 
 <script>
-import { VBtn, VTooltip, VIcon } from 'vuetify/lib';
-
 export default {
   name: 'ResponsiveIconButton',
-  components: { VBtn, VTooltip, VIcon },
   props: {
     breakpoint: {
       required: true,
