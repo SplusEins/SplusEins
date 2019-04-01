998a8d5924195eb49cc57bd53734a07e17a1fbde
schneefux <schneefux+commit@schneefux.xyz>
2019-04-01 13:06:45 +0200

    Remove unused dependencies

--- plugins/vuetify.js
+++ plugins/vuetify.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Vuetify, { VMenu } from 'vuetify/lib';
-import 'vuetify/src/stylus/app.styl';
 import colors from 'vuetify/es5/util/colors';
 
 import moment from 'moment';
