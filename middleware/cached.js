d678922ea46b5b5c2e8d56c3e28ecaeffefe13f7
BrainConverter <38299190+BrainConverter@users.noreply.github.com>
2019-04-15 14:28:38 +0200

    Add theme cookie (#292)
    * Add theme cookie.
    
    * Add Vary: Cookie Header.

--- middleware/cached.js
+++ middleware/cached.js
@@ -1,7 +1,11 @@
 const PAGE_CACHE_SECONDS = process.env.PAGE_CACHE_SECONDS || '600';
 
-export default function ({ res, store }) {
+export default function ({ res, req, store }) {
+  if (req && req.headers && req.headers.cookie && req.headers.cookie.includes('dark=true')) {
+    store.state.ui.isDark = true;
+  }
   if (res != undefined && store.state.errorQueue.length == 0) {
     res.setHeader('Cache-Control', `public, max-age=${PAGE_CACHE_SECONDS}`);
+    res.setHeader('Vary', 'Cookie');
   }
 }
