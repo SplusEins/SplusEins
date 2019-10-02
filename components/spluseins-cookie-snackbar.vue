518ad166259ec9fb4eaf9c7a767a22460cdc9e1e
hidotp <38942179+hidotp@users.noreply.github.com>
2019-10-02 11:01:29 +0200

    added radio buttons for the cookie decision

--- components/spluseins-cookie-snackbar.vue
+++ components/spluseins-cookie-snackbar.vue
@@ -2,46 +2,75 @@
   <v-snackbar
     :value="snackbarOpen"
     :timeout="0"
+    :auto-height="true"
+    :auto-width="true"
     vertical
     right
   >
-    Cookies für Einstellungen und Analytics erlauben?
+    <v-subheader dark>
+      Diese Webseite benutzt Cookies für Einstellungen und Analytics.
+    </v-subheader>
+    Wir benutzen Cookies um die Stundenpläne zu speichern und um die Benutzung der Webseite zu verfolgen. Nur dadurch können wir ein einwandfreies Benutzungserlebnis garantieren.
     <v-layout
-      justify-space-between
       fluid
       row
-    >
-      <v-btn
-        ref="btn-deny"
-        flat
-        @click="setAllCookiesDenied()"
+    > 
+      <v-flex 
+        md10 
+        justify-space-around
       >
-        Nichts
-      </v-btn>
-      <v-btn
-        ref="btn-allow-necessary"
-        flat
-        @click="setNecessaryCookiesAllowed()"
+        <v-radio-group 
+          v-model="cookieDecision" 
+          row 
+          mandatory
+        >
+          <v-radio 
+            dark 
+            color="primary" 
+            label="Keine" 
+            value="nothing" 
+          />
+          <v-radio 
+            dark 
+            color="primary" 
+            label="Einstellungen" 
+            value="settings" 
+          />
+          <v-radio
+            dark 
+            color="primary" 
+            label="Analytics" 
+            value="analytics" 
+          />
+        </v-radio-group>
+      </v-flex>
+      <v-flex 
+        md4
+        justify-end
       >
-        Einstellungen
-      </v-btn>
-      <v-btn
-        ref="btn-allow-all"
-        color="success"
-        flat
-        @click="setAllCookiesAllowed()"
-      >
-        Einverstanden
-      </v-btn>
+        <v-btn
+          ref="btn-set-cookies"
+          flat
+          @click="setCookies()"
+        >
+          Ok
+        </v-btn>
+      </v-flex>
     </v-layout>
   </v-snackbar>
 </template>
 
 <script>
 import { mapMutations, mapState } from 'vuex';
+import { relativeTimeThreshold } from 'moment';
 
 export default {
   name: 'SpluseinsCookieSnackbar',
+  data() {
+    return {
+      cookieDecision: "analytics"
+    };
+  },
   computed: {
     snackbarOpen() {
       return this.browserStateReady && this.allowAllCookies == undefined;
@@ -71,6 +100,15 @@ export default {
       setAllCookiesAllowed: 'privacy/setAllCookiesAllowed',
       setAllCookiesDenied: 'privacy/setAllCookiesDenied',
     }),
+    setCookies(){
+      if(this.cookieDecision.includes("nothing")){
+        this.setAllCookiesDenied()
+      }else if(this.cookieDecision.includes("settings")){
+        this.setNecessaryCookiesAllowed()
+      }else if(this.cookieDecision.includes("analytics")){
+        this.setAllCookiesAllowed()
+      }
+    }
   },
 };
 </script>
