c6fa8ac86e6fdc1e9e3e0497db593808f0a079b5
schneefux <schneefux+commit@schneefux.xyz>
2019-11-18 10:57:59 +0100

    Support Us Card: Add bitcoin

--- components/support-us-card.vue
+++ components/support-us-card.vue
@@ -22,24 +22,68 @@
       <v-btn
         flat
         color="primary"
-        href="http://paypal.me/schneefux"
+        @click="sendMoneyOpen = true"
       >
-        💸 senden (PayPal)
+        <span class="mr-3">💸</span>
+        senden (PayPal)
       </v-btn>
       <v-btn
         flat
         color="primary"
         href="mailto:team@spluseins.de?subject=❤️"
       >
-        ❤️ senden (Mail)
+        <span class="mr-3">❤️</span>
+        senden (Mail)
       </v-btn>
     </v-card-actions>
+
+    <v-bottom-sheet
+      v-model="sendMoneyOpen"
+      inset
+    >
+      <v-list>
+        <v-subheader>Spenden</v-subheader>
+        <v-list-tile
+          href="https://paypal.me/schneefux"
+          @click="sendMoneyOpen = false"
+        >
+          <v-list-tile-avatar>
+            <v-avatar
+              size="32px"
+              tile
+            >
+              <v-icon>mdi-paypal</v-icon>
+            </v-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-title>PayPal</v-list-tile-title>
+        </v-list-tile>
+        <v-list-tile
+          href="bitcoin:3MAZp21eRAo4HjngDSieofWNGVg9tJXg74"
+          @click="sendMoneyOpen = false"
+        >
+          <v-list-tile-avatar>
+            <v-avatar
+              size="32px"
+              tile
+            >
+              <v-icon>mdi-bitcoin</v-icon>
+            </v-avatar>
+          </v-list-tile-avatar>
+          <v-list-tile-title>Bitcoin: 3MAZp21eRAo4HjngDSieofWNGVg9tJXg74</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-bottom-sheet>
   </v-card>
 </template>
 
 <script>
 export default {
   name: 'SupportUsCard',
+  data() {
+    return {
+      sendMoneyOpen: false,
+    }
+  },
 };
 </script>
 
