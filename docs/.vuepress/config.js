43df69715af98bb773ccc4270e0edf80a040f4cf
schneefux <schneefux+commit@schneefux.xyz>
2020-03-05 16:59:39 +0100

    Init vuepress docs

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  title: 'SplusEins',
+  description: 'Dokumentation von spluseins.de',
+  themeConfig: {
+    logo: '/logo.png',
+    sidebar: [
+      ['/', 'Start'],
+      '/konfiguration',
+      '/wartung/',
+      '/wartung/semesterbeginn',
+    ],
+    repo: 'spluseins/spluseins',
+    displayAllHeaders: true,
+    editLinks: true,
+  },
+}
