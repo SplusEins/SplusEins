73c7b9a0588fbd998a08d2ac0a877924f86e8cc5
Thomas <substantialimpulse@pm.me>
2020-11-04 10:25:33 +0100

    Fix error page (should use layout and not be a single page)

--- web/layouts/error.vue
+++ web/layouts/error.vue
@@ -1,94 +1,89 @@
 <template>
-  <v-app :dark="isDark">
-    <spluseins-header />
-    <v-content fill-height>
-      <v-layout
-        align-center
-        justify-center
-        column
-        fill-height
+  <v-container
+    v-touch="{
+      right: () => setSidenav(true),
+    }"
+    class="container"
+  >
+    <v-layout
+      align-center
+      justify-center
+      column
+      fill-height
+    >
+      <v-card
+        pa-2
+        ma-2
+        width="50%"
       >
-        <v-card
-          pa-2
-          ma-2
-          width="50%"
-        >
-          <v-card-media
+        <v-img
+          v-if="error.statusCode == 404"
+          class="ErrorImg"
+          src="/404.png"
+          width="80%"
+          position="right"
+        />
+        <v-img
+          v-else-if="error.statusCode == 403 || error.statusCode == 401"
+          class="ErrorImg"
+          src="/403_401.png"
+          width="80%"
+          position="right"
+        />
+
+        <v-img
+          v-else-if="error.statusCode == 500"
+          class="ErrorImg"
+          src="/500.png"
+          width="80%"
+          position="right"
+        />
+
+        <v-card-text>
+          <div
             v-if="error.statusCode == 404"
-            class="ErrorImg"
-            src="/404.png"
-            width="80%"
-            position="right"
-          />
-          <v-card-media
-            v-else-if="error.statusCode == 403 || 401"
-            class="ErrorImg"
-            src="/403_401.png"
-            width="80%"
-            position="right"
-          />
+            class="text-xs-center"
+          >
+            Diese Seite gibt es leider nicht...
+          </div>
+          <div
+            v-if="error.statusCode == 403"
+            class="text-xs-center"
+          >
+            Verbinde dich mit dem Ostfalia-WLAN, bevor du diesen Plan aufrufen
+            kannst.
+          </div>
+        </v-card-text>
 
-          <v-card-media
-            v-else-if="error.statusCode == 500"
-            class="ErrorImg"
-            src="/500.png"
-            width="80%"
-            position="right"
-          />
+        <v-card-text>
+          <div
+            v-if="error.statusCode == 500"
+            class="text-xs-center"
+          >
+            Internal Server Error
+          </div>
+        </v-card-text>
 
-          <v-card-text>
-            <div
-              v-if="error.statusCode == 404"
-              class="text-xs-center"
-            >
-              Diese Seite gibt es leider nicht...
-            </div>
-            <div
-              v-if="error.statusCode == 403"
-              class="text-xs-center"
-            >
-              Verbinde dich mit dem Ostfalia-WLAN, bevor du diesen Plan aufrufen kannst.
-            </div>
-          </v-card-text>
-
-          <v-card-text>
-            <div
-              v-if="error.statusCode == 500"
-              class="text-xs-center"
-            >
-              Internal Server Error
-            </div>
-          </v-card-text>
-
-          <v-card-actions>
-            <v-btn
-              flat
-              to="/"
-              nuxt
-              color="primary"
-              @click="$track('Error', 'backToMain', 'error status', error.statusCode)"
-            >
-              Main Page
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-layout>
-    </v-content>
-    <spluseins-footer />
-  </v-app>
+        <v-card-actions>
+          <v-btn
+            flat
+            to="/"
+            nuxt
+            color="primary"
+            @click="$track('Error', 'backToMain', 'error status', error.statusCode)"
+          >
+            Main Page
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-
-import SpluseinsHeader from '../components/spluseins-header.vue';
-import SpluseinsFooter from '../components/spluseins-footer.vue';
+import { mapState } from "vuex";
 
 export default {
-  components: {
-    SpluseinsHeader,
-    SpluseinsFooter,
-  },
   props: {
     error: {
       type: Object,
