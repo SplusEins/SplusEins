fe040f7c5d00ad176d86edd6cbfa080482b3e14e
Thomas <substantialimpulse@pm.me>
2021-01-28 12:56:11 +0100

    Party revert index page changes and fix spelling

--- web/components/last-changes-card.vue
+++ web/components/last-changes-card.vue
@@ -7,10 +7,9 @@
     </v-card-title>
     <v-card-text class="card-text-padding">
       <ul>
-        <li>Dieser Server wird jetzt von der Fakultät Informatik zur Verfügung gestellt, weshalb wir auch direkt unter <a href="https://spluseins-i.ostfalia.de">spluseins-i.ostfalia.de</a> erreichbar sind.</li>
-        <li>Da die Serverkosten jetzt von der Fakultät getragen werden, haben wir die Spendenmöglichkeit und leider auch die Liste der Spenden entfernt. Trotzdem möchten wir uns hier nochmal bei Kevin, Florian H., Paul, Lea, Florian B. und den vielen anderen bedanken, die uns mit Spenden und freundlichen Nachrichten motiviert haben &lt;3</li>
-        <li>Die Pläne können jetzt automatisiert hinzugefügt werden, sodass die Pläne ab nächstes Semester wieder rechtzeitig zur Verfügung stehen. </li>
-        <li>Wenn eure Fakultät noch fehlt, liegt das vermutlich daran, dass die Fakultät die Pläne nur per PDF zur Verfügung steht. Diese können leider nicht in SplusEins angezeigt werden.</li>
+        <li>Dieser Server wird jetzt von der Fakultät Informatik zur Verfügung gestellt, weshalb wir auch unter <a href="https://spluseins-i.ostfalia.de">spluseins-i.ostfalia.de</a> erreichbar sind.</li>
+        <li>Da die Serverkosten von der Fakultät getragen werden, haben wir die Spendenmöglichkeit entfernt. Trotzdem möchten wir uns hier nochmal bei Kevin, Florian H., Paul, Lea, Florian B. und den vielen anderen bedanken, die uns mit Spenden und freundlichen Nachrichten motiviert haben &lt;3</li>
+        <li>Wenn eure Fakultät die Pläne nur als PDF zur Verfügung stellt, können diese leider nicht in SplusEins angezeigt werden.</li>
       </ul>
     </v-card-text>
   </v-card>
