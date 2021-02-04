1579b193b22f602faf9b6b27484e9b2632fc3a0e
Thomas <substantialimpulse@pm.me>
2021-02-04 14:25:48 +0100

    build only on pushes to master & PR, remove serverless leftovers

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,48 +1,5 @@
 const path = require('path');
 
-const serverlessConfig = {
-  mode: process.env.NODE_ENV,
-  target: 'node',
-  devtool: false,
-  entry: {
-    api: './serverless.ts',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, './dist/serverless'),
-    libraryTarget: 'commonjs',
-  },
-  module: {
-    rules: [ {
-      test: /\.ts$/,
-      use: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                { targets: { node: '8.15.0' } },
-              ],
-            ],
-            plugins: [
-              '@babel/plugin-proposal-class-properties',
-              '@babel/plugin-transform-object-assign',
-              '@babel/plugin-proposal-object-rest-spread'
-            ]
-          }
-        },
-        { loader: 'ts-loader' },
-      ],
-      exclude: /node_modules/
-    } ]
-  },
-  resolve: {
-    extensions: [ '.ts', '.js' ],
-    mainFields: [ 'main' ],
-  },
-};
-
 const serverConfig = {
   mode: process.env.NODE_ENV,
   target: 'node',
@@ -68,4 +25,4 @@ const serverConfig = {
   },
 };
 
-module.exports = [ serverlessConfig, serverConfig ];
+module.exports = [ serverConfig ];
