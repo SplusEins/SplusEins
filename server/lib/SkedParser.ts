33abba965b334a352f88a582bb0f7048b4dacc37
carlchrhaensel <carlchrhaensel@users.noreply.github.com>
2025-02-22 21:50:08 +0000

    feat: implement parseTimetableIntranet

--- server/lib/SkedParser.ts
+++ server/lib/SkedParser.ts
@@ -6,7 +6,7 @@ import * as CSV from 'csv-string';
 
 moment.locale('de');
 
-export function parseSkedList (html: string): ParsedLecture[] {
+export function parseSkedList(html: string): ParsedLecture[] {
   const $ = load(html);
 
   const events = [] as ParsedLecture[];
@@ -44,7 +44,7 @@ export function parseSkedList (html: string): ParsedLecture[] {
         dozent = cols[7];
         raum = cols[9];
         anmerkung = cols[11];
-        break
+        break;
       case 15:
         datum = cols[0] || lastDatum;
         uhrzeitStart = cols[2];
@@ -53,7 +53,7 @@ export function parseSkedList (html: string): ParsedLecture[] {
         dozent = cols[7];
         raum = cols[9];
         anmerkung = cols[13];
-        break
+        break;
       case 17:
         datum = cols[5] || lastDatum;
         uhrzeitStart = cols[0];
@@ -62,13 +62,13 @@ export function parseSkedList (html: string): ParsedLecture[] {
         dozent = cols[3];
         raum = cols[9];
         anmerkung = cols[15];
-        break
+        break;
       default:
         /**
          * Does not mean that an error occured.
          * Possibly a wrong row is parsed.
          */
-        return
+        return;
     }
 
     lastDatum = datum;
@@ -101,7 +101,7 @@ export function parseSkedList (html: string): ParsedLecture[] {
   return events;
 }
 
-export function parseSkedCSV (csvString: string): ParsedLecture[] {
+export function parseSkedCSV(csvString: string): ParsedLecture[] {
   const events = [] as ParsedLecture[];
 
   const arr = CSV.parse(csvString);
@@ -127,7 +127,7 @@ export function parseSkedCSV (csvString: string): ParsedLecture[] {
           datum = content;
           break;
         case 'uhrzeit': {
-          const timeParts = content.split('Uhr')[0].split(' - ')
+          const timeParts = content.split('Uhr')[0].split(' - ');
           uhrzeitStart = timeParts[0].trim();
           uhrzeitEnde = timeParts[1].trim();
           break;
@@ -177,62 +177,62 @@ export function parseSkedCSV (csvString: string): ParsedLecture[] {
   return events;
 }
 
-export function parseSkedGraphical (html: string, faculty: string): ParsedLecture[] {
+export function parseSkedGraphical(html: string, faculty: string): ParsedLecture[] {
   const $ = load(html);
   const events = [] as ParsedLecture[];
 
   $('body table').each(function () {
-    let cols = parseTable(load(this, { decodeEntities: false }), true, true, false)
+    let cols = parseTable(load(this, { decodeEntities: false }), true, true, false);
 
-    cols = cols.filter(c => c[0].length > 0) // filter pseudo columns -> first entry is not date string
+    cols = cols.filter(c => c[0].length > 0); // filter pseudo columns -> first entry is not date string
     cols = cols.map(col => col.filter(text =>
-      !new RegExp('^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$', 'g').test(text) && // filter time entries
-        text.length > 1 && // filter empty entries and empty tags -> 1
-        !text.startsWith('['))) // filter footnotes
-    cols = cols.filter(col => col.length > 1) // filter empty
-    cols = cols.map(col => [...new Set(col)]) // filter duplicates
+      !/^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/g.test(text) && // filter time entries
+      text.length > 1 && // filter empty entries and empty tags -> 1
+      !text.startsWith('['))); // filter footnotes
+    cols = cols.filter(col => col.length > 1); // filter empty
+    cols = cols.map(col => [...new Set(col)]); // filter duplicates
     cols = cols.map(col => col.map(text => text
       .replace(/<span.+>.+<\/span>/g, '') // remove <span>
       .replace(/\s\[\d+\]/g, ''))); // replace footnote links: e.g. [1]
 
     cols.forEach(col => {
       col.forEach((entry, index) => {
-        if (index == 0) {
+        if (index === 0) {
           // is date
           return;
         }
-        const datum = col[0].split(', ')[1]
+        const datum = col[0].split(', ')[1];
         const parts = entry.split('<br>').map(part => part.replace(/<a.+>(.+)<\/a>/, '$1'));
         if (parts.length < 3) {
           // Should at least have time & name, otherwise no valid column
           return;
         }
-        const time = parts[0].split('Uhr')[0].split(' - ')
+        const time = parts[0].split('Uhr')[0].split(' - ');
         let uhrzeitStart = time[0];
         let uhrzeitEnde = time[1];
 
-        let dozent = ''
-        let veranstaltung = ''
-        let raum = ''
-        let anmerkung = ''
+        let dozent = '';
+        let veranstaltung = '';
+        let raum = '';
+        let anmerkung = '';
 
         switch (faculty) {
           case 'Versorgungstechnik':
             dozent = parts[1];
             veranstaltung = parts[2];
-            raum = parts[3]
-            anmerkung = parts.splice(4).join(', ') || ''
+            raum = parts[3];
+            anmerkung = parts.splice(4).join(', ') || '';
             break;
           case 'Recht':
             dozent = parts[3];
             veranstaltung = parts[2] + ' ' + parts[1];
-            raum = parts[4]
-            anmerkung = parts.splice(5).join(', ') || ''
+            raum = parts[4];
+            anmerkung = parts.splice(5).join(', ') || '';
             break;
           case 'Bau-Wasser-Boden':
             dozent = parts[3];
             veranstaltung = parts[2];
-            raum = parts[4]
+            raum = parts[4];
             anmerkung = parts[1];
             break;
           case 'Handel und Soziale Arbeit':
@@ -246,21 +246,21 @@ export function parseSkedGraphical (html: string, faculty: string): ParsedLectur
             break;
           case 'Soziale Arbeit':
             if (parts[0].endsWith('Uhr')) {
-              parts.shift() // remove first uhrzeit entry
+              parts.shift(); // remove first uhrzeit entry
             }
             while (parts[0] !== undefined && !parts[0].startsWith('S-') && !parts[0].startsWith('Ringveranstaltung')) {
-              anmerkung += parts.shift() + ', '
+              anmerkung += parts.shift() + ', ';
             }
             if (parts.length > 3) {
-              anmerkung += parts.shift() + ', '
+              anmerkung += parts.shift() + ', ';
               dozent = parts.shift();
               raum = parts.pop();
             }
-            anmerkung += parts.join(', ') || ''
+            anmerkung += parts.join(', ') || '';
             anmerkung = anmerkung.replace(/(, ?)+$/, ''); // remove trailing ,
 
             // einige veranstaltungen enthalten den dozenten im titel
-            veranstaltung = veranstaltung.replace(` - ${dozent}`, '').replace(` ${dozent}`, '')
+            veranstaltung = veranstaltung.replace(` - ${dozent}`, '').replace(` ${dozent}`, '');
             break;
           case 'Fahrzeugtechnik':
             raum = parts[0];
@@ -279,16 +279,16 @@ export function parseSkedGraphical (html: string, faculty: string): ParsedLectur
             } else {
               dozent = parts[2];
               veranstaltung = parts[1];
-              raum = parts[3]
-              anmerkung = parts.splice(4).join(', ') || ''
+              raum = parts[3];
+              anmerkung = parts.splice(4).join(', ') || '';
             }
             break;
           default:
-            console.log('No parser defined for faculty ' + faculty)
+            console.log('No parser defined for faculty ' + faculty);
             throw new Error('This faculty is not supported.');
         }
 
-        const dateFormat = 'DD.MM.YYYY H:m'
+        const dateFormat = 'DD.MM.YYYY H:m';
         const start = moment.tz(datum + ' ' + uhrzeitStart, dateFormat, 'Europe/Berlin');
         const end = moment.tz(datum + ' ' + uhrzeitEnde, dateFormat, 'Europe/Berlin');
         if (!start.isValid() || !end.isValid() || veranstaltung === '') {
@@ -304,14 +304,59 @@ export function parseSkedGraphical (html: string, faculty: string): ParsedLectur
           end: end.utc().toDate(),
           duration: end.diff(start, 'hours', true)
         } as ParsedLecture);
-      })
-    })
+      });
+    });
   });
 
   return events;
 }
 
-export function parseTimetableIntranet (html: string): ParsedLecture[] {
-  // TODO: Implement intranet timetable parsing
-  throw new Error('Das neue Stundenplanformat für Informatik ist noch nicht in SplusEins implementiert. Bitte habt noch etwas Geduld.');
+export function parseTimetableIntranet(html: string): ParsedLecture[] {
+  const $ = load(html);
+  const days = $('table.schedule');
+  const events = [] as ParsedLecture[];
+  days.each(function (index, day) {
+    const date = $(day).find('.day').html().split('<br>')[1];
+    const jsDate = moment(date, 'DD.MM.YYYY').toDate();
+
+    $(day).find('.day-event').each(function (i, event) {
+      const styleTop = parseInt($(event).css('top'));
+      const styleHeight = parseInt($(event).css('height'));
+
+      // timetable starts at 8:00
+      // 100px height = 60 minutes
+      const startTimeSince8 = styleTop / 100 * 60;
+      const endTimeSince8 = (styleTop + styleHeight) / 100 * 60;
+
+      const eventStart = new Date(jsDate);
+      eventStart.setHours(8 + Math.floor(startTimeSince8 / 60));
+      eventStart.setMinutes(startTimeSince8 % 60);
+
+      const eventEnd = new Date(jsDate);
+      eventEnd.setHours(8 + Math.floor(endTimeSince8 / 60));
+      eventEnd.setMinutes(endTimeSince8 % 60);
+
+      const title = $(event).find('.text').text();
+      const room = $(event).find('.rooms').text();
+
+      const dateFormat = 'DD.MM.YYYY H:m';
+      const start = moment.tz(date + ' ' + eventStart.toTimeString(), dateFormat, 'Europe/Berlin');
+      const end = moment.tz(date + ' ' + eventEnd.toTimeString(), dateFormat, 'Europe/Berlin');
+      if (!start.isValid() || !end.isValid()) {
+        return;
+      }
+
+      events.push({
+        info: '',
+        room,
+        lecturer: '',
+        title,
+        start: start.utc().toDate(),
+        end: end.utc().toDate(),
+        duration: end.diff(start, 'hours', true)
+      });
+    });
+  });
+
+  return events;
 }
