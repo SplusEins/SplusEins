21f85520cf350cf01542af1d524dc32d93cd93cd
Dennis Haumann <66166638+Dennis960@users.noreply.github.com>
2025-09-10 19:41:46 +0200

    Revert "Revert "Hafas motis fptf client swap (#1053)" (#1058)" (#1060)
    This reverts commit c6a310433f1dd80c6429fc7701ac71057d392c55.

--- web/store/bus.js
+++ web/store/bus.js
@@ -1,26 +1,30 @@
 export const state = () => ({
-  departures: null
+  departures: null,
+  lastUpdated: null
 });
 
 export const mutations = {
   setDepartures (state, data) {
-    state.departures = data;
+    state.departures = data.departures;
+    state.lastUpdated = data.lastUpdated;
   }
 }
 
 export const actions = {
   async load ({ commit }) {
-    let result = [];
+    let departures = [];
+    let lastUpdated = null;
 
     try {
       const response = await this.$axios.get('/api/bus');
-      result = response.data;
+      departures = response.data.departures;
+      lastUpdated = response.data.lastUpdated;
     } catch (error) {
       const errorCode = error.response ? error.response.status : 'unknown error'
       commit('enqueueError', `Busplan konnte nicht geladen werden (${errorCode}).`, { root: true });
       console.error('error during Bus API call', error.message);
     }
 
-    commit('setDepartures', result);
+    commit('setDepartures', { departures, lastUpdated });
   }
 };
