8542fd443af67eb60bd068ac07985a6347aa306d
schneefux <schneefux+commit@schneefux.xyz>
2025-10-13 11:20:42 +0200

    Update app

---
+++ app/app/src/main/java/de/spluseins/app/DelegationService.java
@@ -0,0 +1,14 @@
+package de.spluseins.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
