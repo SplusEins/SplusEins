19a01702b7684b5b8dbd3704951ab62b5b217175
jakob-rzeppa <rzeppa.jakob@gmail.com>
2025-11-19 08:26:28 +0100

    updated getLocationLinks script to include main building data

--- server/scripts/getLocationLinks.sh
+++ server/scripts/getLocationLinks.sh
@@ -2,6 +2,6 @@
 
 # Rerun after changes to OSM data (e. g. adding new rooms or changing existing ones)
 
-DATA_QUERY='[out:json][timeout:25]; nwr["indoor"="room"]["ref"~"WF-EX"](52.1774,10.5574,52.1806,10.5624); out tags geom;'
+DATA_QUERY='[out:json][timeout:25]; nwr["indoor"="room"]["ref"~""](52.174312,10.542111,52.183790,10.564728); out tags geom;'
 
 curl -X GET --data-urlencode "data=${DATA_QUERY}" "https://overpass-api.de/api/interpreter" | jq '.elements | map({id: .id, level: .tags.level, ref: .tags.ref}) | map({(.ref): {id: .id, level: .level}}) | add' > ../assets/overpassOSMRoomsData.json
