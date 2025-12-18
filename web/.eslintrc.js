35dbf9790ccba780cd3423810241b7fe65091767
Thomas <substantialimpulse@pm.me>
2025-12-18 15:18:39 +0100

    chore: add prettier as formatter

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -7,17 +7,15 @@ module.exports = {
     'eslint:recommended',
     'plugin:vue/strongly-recommended',
     'plugin:nuxt/recommended',
-    'standard'
+    'standard',
+    'prettier'
   ],
   // required to lint *.vue files
   plugins: ['vue'],
   // add your custom rules here
   rules: {
-    'multiline-ternary': 'warn',
     eqeqeq: 'warn', // don't error on wrong equality signs
     camelcase: 'warn', // don't error on non camel case
-    quotes: 'warn', // don't error on double qoutes
-    'comma-dangle': 'warn',
     'spaced-comment': 'warn',
     semi: 'off', // no preference for semicolons at end
     complexity: ['warn', { max: 30 }],
