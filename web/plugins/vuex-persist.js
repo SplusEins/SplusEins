96b8adc30abde1516d6aec3f2dae0e2a1487e9dd
DrR955 <78657761+DrR955@users.noreply.github.com>
2026-08-09 21:11:45 +0200

    chore: semesterwechsel auf ws26/27 (#1243)
    * chore: update SEMESTER_WEEK_1 for WS 26/27
    
    * chore: update current_sem and blacklist
    
    * chore: increment store version for ws 26/27
    
    * Updated timetables with sked-parser
    
    * chore: add "ss 202" to timetable blacklist
    
    ---------
    
    Co-authored-by: actions-user <actions@github.com>

--- web/plugins/vuex-persist.js
+++ web/plugins/vuex-persist.js
@@ -15,12 +15,12 @@ export default ({ store }) => {
 
         // version migration
         if (value.version) {
-          if (value.version < 17) {
+          if (value.version < 18) {
             // TODO Semesterwechsel increment this
             value.splus.customSchedules = {};
             value.splus.favoriteSchedules = [];
             value.splus.subscribedTimetable = {};
-            value.version = 17; // TODO Semesterwechsel increment this
+            value.version = 18; // TODO Semesterwechsel increment this
           }
         }
 
